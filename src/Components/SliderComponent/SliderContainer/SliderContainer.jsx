306206e22e8645aed55b740723158990f223cdab
MedEBoudali <med.sneaky@gmail.com>
2020-07-29 00:03:45 +0000

    add dots

--- src/Components/SliderComponent/SliderContainer/SliderContainer.jsx
+++ src/Components/SliderComponent/SliderContainer/SliderContainer.jsx
@@ -3,17 +3,19 @@ import { Slider, SliderWrapper, SliderItem } from './SliderContainer.Style';
 import Arrow from '../Arrow/Arrow';
 
 const SliderContainer = () => {
-  const getWidth = () => window.innerWidth;
-  const [SlideAnimation, setSlideAnimation] = useState(true);
+  const [CurrentIndex, setCurrentIndex] = useState(0);
   const [translate, setTranslate] = useState(0);
-  const [Images] = useState([
+  const [SlideAnimation, setSlideAnimation] = useState(true);
+  const getWidth = () => window.innerWidth;
+
+  const Images = [
     'https://images.unsplash.com/photo-1514509897857-fb32c118ca22?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1498&q=80',
     'https://images.unsplash.com/photo-1562478899-f8bca5c85ca8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
     'https://images.unsplash.com/photo-1580813089076-7a92471e7074?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1424&q=80',
     'https://images.unsplash.com/photo-1566664022077-4dc1446dd619?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1510&q=80'
-  ]);
-  const [CurrentIndex, setCurrentIndex] = useState(0);
+  ];
   const imagesLength = Images.length;
+
   const nextSlide = useCallback(() => {
     if (CurrentIndex === imagesLength - 1) {
       setTranslate(0);
